47b2b6bbea5bf342f9a469b5871a046882bb8a3b
CroxxN <saradgaihre19@gmail.com>
2022-12-21 16:55:31 +0545

    add structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 mod ui;
 
 use libpulse_binding as pulse;
@@ -11,46 +12,68 @@ use pulse::proplist::Proplist;
 // use pulse::stream::Direction;
 // use pulse::volume::ChannelVolumes;
 
-fn main() {
-    let mut mainloop = Mainloop::new().expect("Failed to create mainloop");
-    let mut context =
-        Context::new_with_proplist(&mainloop, "pulsar", &Proplist::new().unwrap()).unwrap();
+struct Pulse {
+    mainloop: Mainloop,
+    context: Context,
+}
 
-    context
-        .connect(None, pulse::context::FlagSet::NOFLAGS, None)
-        .unwrap();
-    context.set_state_callback(Some(Box::new(|| {
-        println!("Connected");
-    })));
-    loop {
-        if context.get_state() == pulse::context::State::Ready {
-            context.set_state_callback(None);
-            println!("Ready");
-            break;
-        } else {
-            mainloop.iterate(false);
+impl Pulse {
+    fn initiate() -> Pulse {
+        let mut mainloop = Mainloop::new().expect("Failed to create mainloop");
+        let mut context = Context::new_with_proplist(
+            &mainloop,
+            "pulsar",
+            &Proplist::new().expect("Failed to create the proplist"),
+        )
+        .expect("Failed to create the context");
+
+        context
+            .connect(None, pulse::context::FlagSet::NOFLAGS, None)
+            .unwrap();
+        context.set_state_callback(Some(Box::new(|| {
+            println!("Connected to pulse backend");
+        })));
+        loop {
+            if context.get_state() == pulse::context::State::Ready {
+                context.set_state_callback(None);
+                println!("Ready");
+                break;
+            } else {
+                mainloop.iterate(false);
+            }
         }
+        Pulse { mainloop, context }
     }
+    fn set_subscribe(&mut self) {
+        let flags = InterestMaskSet::SINK_INPUT;
 
-    let flags = InterestMaskSet::SINK_INPUT;
-
-    context.subscribe(flags, |res| {
-        println!("Subscribed: {:?}", res);
-    });
-    context.set_subscribe_callback(Some(Box::new(|_, _, index| {
-        println!("{index}");
-    })));
-
-    context
-        .introspect()
-        .get_sink_input_info_list(|list| match list {
-            ListResult::Item(value) => {
-                println!("Sink {:?}", value.proplist);
-            }
-            _ => {
-                println!("No sink found")
-            }
+        self.context.subscribe(flags, |res| {
+            println!("Subscription: {:?}", res);
         });
-
-    mainloop.run().unwrap();
+    }
+    fn run(&mut self) {
+        self.mainloop.run().expect("Unable to run the mainloop");
+    }
+    fn get_sink_list(&mut self) {
+        self.context
+            .set_subscribe_callback(Some(Box::new(|_x, _y, index| {
+                // ctx.introspect()
+                //     .get_sink_info_by_index(index, |list| match list {
+                //         ListResult::Item(value) => {
+                //             println!("Sink {:?}", value.proplist);
+                //         }
+                //         _ => {
+                //             println!("No sink found")
+                //         }
+                //     });
+                // println!("{index}");
+            })));
+    }
+}
+fn main() {
+    let mut ctx = Pulse::initiate();
+    ctx.run();
+    // context
+    //     .introspect()
+    //     .get_sink_input_info_list();
 }
